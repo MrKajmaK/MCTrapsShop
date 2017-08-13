178155b6c90f7e38f24ae07b9085ef9e04e73441
KajmaczeK <kajmaczek@kajmaczek.pl>
2017-08-14 01:34:51 +0200

    Travis: directories...

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rsync -tv /home/travis/build/MrKajmaK/MCTrapsShop/target/MCTrapsShop.jar $FTP_USER@5.135.248.254:home/mcec/mcecroot/plugins
+rsync -tv /home/travis/build/MrKajmaK/MCTrapsShop/target/MCTrapsShop.jar $FTP_USER@5.135.248.254:/home/mcec/mcecroot/plugins
