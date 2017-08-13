04c1a74c67d75414a7bad028ec23a0dbdec31137
arizona21 <30986187+arizona21@users.noreply.github.com>
2017-08-13 22:48:08 +0200

    Update MCTrapsShopCommandExecutor.java

--- src/eu/mctraps/shop/MCTrapsShopCommandExecutor.java
+++ src/eu/mctraps/shop/MCTrapsShopCommandExecutor.java
@@ -1,118 +1,123 @@
-package eu.mctraps.shop;
+/*     */ package eu.mctraps.shop;
+/*     */ 
+/*     */ import java.sql.ResultSet;
+/*     */ import java.sql.SQLException;
+/*     */ import java.sql.Statement;
+/*     */ import java.text.SimpleDateFormat;
+/*     */ import java.util.ArrayList;
+/*     */ import java.util.List;
+/*     */ import org.bukkit.command.Command;
+/*     */ import org.bukkit.command.CommandExecutor;
+/*     */ import org.bukkit.command.CommandSender;
+/*     */ import org.bukkit.configuration.file.FileConfiguration;
+/*     */ 
+/*     */ public class MCTrapsShopCommandExecutor implements CommandExecutor
+/*     */ {
+/*     */   private final MCTrapsShop plugin;
+/*     */   
+/*     */   public MCTrapsShopCommandExecutor(MCTrapsShop plugin)
+/*     */   {
+/*  20 */     this.plugin = plugin;
+/*     */   }
+/*     */   
+/*     */   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+/*     */   {
+/*  25 */     if (cmd.getName().equalsIgnoreCase("smsshop")) {
+/*  26 */       if (args.length == 0) {
+/*  27 */         sender.sendMessage("§7Glowne komendy pluginu:");
+/*  28 */         sender.sendMessage(" §7/voucher §6<list;add;remove>");
+/*     */         
+/*  30 */         return true;
+/*     */       }
+/*  32 */       return false;
+/*     */     }
+/*  34 */     if (cmd.getName().equalsIgnoreCase("voucher")) {
+/*  35 */       if ((!(sender instanceof org.bukkit.entity.Player)) || (sender.hasPermission("tools.voucher"))) {
+/*  36 */         String vTable = this.plugin.config.getString("tables.vouchers");
+/*  37 */         String oTable = this.plugin.config.getString("tables.offers");
+/*  38 */         String hTable = this.plugin.config.getString("tables.history");
+/*     */         
+/*  40 */         if (args.length == 0) {
+/*  41 */           return false;
+/*     */         }
+/*  43 */         if (args[0].equalsIgnoreCase("list")) {
+/*  44 */           if (args.length == 1) {
+/*     */             try {
+/*  46 */               ResultSet result = this.plugin.statement.executeQuery("SELECT * FROM " + vTable);
+/*  47 */               List<String> vouchers = new ArrayList();
+/*  48 */               while (result.next()) {
+/*  49 */                 String code = result.getString("code");
+/*  50 */                 int uses = result.getInt("uses");
+/*  51 */                 int id = result.getInt("id");
+/*  52 */                 String voucher = " §a" + code + " (#" + id + ")";
+/*  53 */                 vouchers.add(voucher);
+/*     */               }
+/*     */               
+/*  56 */               sender.sendMessage("§7Lista aktualnych voucherów:");
+/*  57 */               for (int i = 0; i < vouchers.size(); i++) {
+/*  58 */                 sender.sendMessage((String)vouchers.get(i));
+/*     */               }
+/*  60 */               sender.sendMessage("§9Aby dowiedziec sie wiecej o danym voucherze, uzyj komendy: §7/voucher info §6<id>");
+/*     */             } catch (SQLException e) {
+/*  62 */               e.printStackTrace();
+/*  63 */               sender.sendMessage("§cWystapil blad podczas laczenia z baza danych");
+/*     */             }
+/*     */             
+/*  66 */             return true;
+/*     */           }
+/*  68 */           sender.sendMessage("§cPoprawne uzycie: §7/voucher list");
+/*  69 */           return true;
+/*     */         }
+/*  71 */         if ((args[0].equalsIgnoreCase("info")) && 
+/*  72 */           (args.length == 2)) {
+/*     */           try {
+/*  74 */             ResultSet result = this.plugin.statement.executeQuery("SELECT * FROM " + vTable + " WHERE id = '" + args[1] + "' ORDER BY uses DESC LIMIT 1");
+/*  75 */             int id = 0;int uses = 0;int offerid = 0;int timed = 0;
+/*  76 */             String code = "";String timedText = "";String end = "";
+/*  77 */             while (result.next()) {
+/*  78 */               id = result.getInt("id");
+/*  79 */               code = result.getString("code");
+/*  80 */               uses = result.getInt("uses");
+/*  81 */               offerid = result.getInt("offer");
+/*  82 */               timed = result.getInt("timed");
+/*  83 */               timedText = timed == 1 ? "tak" : "nie";
+/*  84 */               end = new SimpleDateFormat("dd/MM/yyyy HH:mm").format(result.getTimestamp("endtime"));
+/*     */             }
+/*     */             
+/*  87 */             ResultSet offerResult = this.plugin.statement.executeQuery("SELECT * FROM " + oTable + " WHERE id = '" + offerid + "' LIMIT 1");
+/*  88 */             String offername = "Brak usługi o takim ID";
+/*  89 */             while (offerResult.next()) {
+/*  90 */               offername = offerResult.getString("name");
+/*     */             }
+/*     */             
+/*  93 */             sender.sendMessage("§7Informacje o voucherze §6#" + id);
+/*  94 */             sender.sendMessage(" §9Kod: §7" + code);
+/*  95 */             sender.sendMessage(" §9Pozostale uzycia: §7" + uses);
+/*  96 */             sender.sendMessage(" §9Kod do uslugi: §7#" + offerid + " §7(§c" + offername + "§7)");
+/*  97 */             sender.sendMessage(" §9Oferta ograniczona czasowo?: §7" + timedText);
+/*  98 */             if (timed == 1) {
+/*  99 */               sender.sendMessage(" §9Oferta wazna do: §7" + end);
+/*     */             }
+/*     */             
+/* 102 */             return true;
+/*     */           } catch (SQLException e) {
+/* 104 */             e.printStackTrace();
+/* 105 */             sender.sendMessage("§cWystapil blad w trakcie laczenia z baza danych");
+/* 106 */             return true;
+/*     */           }
+/*     */         }
+/*     */       }
+/*     */       else {
+/* 111 */         sender.sendMessage("§cNie masz dostepu! :(");
+/*     */       }
+/*     */     }
+/*     */     
+/* 115 */     return false;
+/*     */   }
+/*     */ }
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-public class MCTrapsShopCommandExecutor implements CommandExecutor {
-    private final MCTrapsShop plugin;
-
-    public MCTrapsShopCommandExecutor(MCTrapsShop plugin) {
-        this.plugin = plugin;
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if(cmd.getName().equalsIgnoreCase("smsshop")) {
-            if(args.length == 0) {
-                sender.sendMessage("§c__-- §6Komendy pluginu: §c--__");
-                sender.sendMessage("  §7- §9/voucher <list|add|remove>");
-
-                return true;
-            } else {
-                return false;
-            }
-        } else if(cmd.getName().equalsIgnoreCase("voucher")) {
-            if(!(sender instanceof Player) || sender.hasPermission("smsshop.voucher")) {
-                String vTable = plugin.config.getString("tables.vouchers");
-                String oTable = plugin.config.getString("tables.offers");
-                String hTable = plugin.config.getString("tables.history");
-
-                if (args.length == 0) {
-                    return false;
-                }
-                if (args[0].equalsIgnoreCase("list")) {
-                    if (args.length == 1) {
-                        try {
-                            ResultSet result = plugin.statement.executeQuery("SELECT * FROM " + vTable);
-                            List<String> vouchers = new ArrayList<>();
-                            while (result.next()) {
-                                String code = result.getString("code");
-                                int uses = result.getInt("uses");
-                                int id = result.getInt("id");
-                                String voucher = (uses == 0) ? "  §7- " + code + " (#" + id + ")" : "  §7- §2" + code + " (#" + id + ")";
-                                vouchers.add(voucher);
-                            }
-
-                            sender.sendMessage("§c__-- §6Lista voucherów: §c--__");
-                            for (int i = 0; i < vouchers.size(); i++) {
-                                sender.sendMessage(vouchers.get(i));
-                            }
-                            sender.sendMessage("§c__-- §dAby dowiedzieć się więcej o danym voucherze, użyj /voucher info <id> §c--__");
-                        } catch (SQLException e) {
-                            e.printStackTrace();
-                            sender.sendMessage("§cWystąpił błąd w trakcie łączenia z bazą danych");
-                        }
-
-                        return true;
-                    } else {
-                        sender.sendMessage("§cPoporawny sposób użycia: §6/voucher list");
-                        return true;
-                    }
-                } else if (args[0].equalsIgnoreCase("info")) {
-                    if (args.length == 2) {
-                        try {
-                            ResultSet result = plugin.statement.executeQuery("SELECT * FROM " + vTable + " WHERE id = '" + args[1] + "' ORDER BY uses DESC LIMIT 1");
-                            int id = 0, uses = 0, offerid = 0, timed = 0;
-                            String code = "", timedText = "", end = "";
-                            while(result.next()) {
-                                id = result.getInt("id");
-                                code = result.getString("code");
-                                uses = result.getInt("uses");
-                                offerid = result.getInt("offer");
-                                timed = result.getInt("timed");
-                                timedText = (timed == 1) ? "tak" : "nie";
-                                end = new SimpleDateFormat("dd/MM/yyyy HH:mm").format(result.getTimestamp("endtime"));
-                            }
-
-//                          ToDo: check if voucher exists, subcommands
-
-                            ResultSet offerResult = plugin.statement.executeQuery("SELECT * FROM " + oTable + " WHERE id = '" + offerid + "' LIMIT 1");
-                            String offername = "Brak usługi o takim ID";
-                            while(offerResult.next()) {
-                                offername = offerResult.getString("name");
-                            }
-
-                            sender.sendMessage("§c__-- §6Informacje o voucherze #" + id + " §c--__");
-                            sender.sendMessage("§6Kod: §2" + code);
-                            sender.sendMessage("§6Pozostałe użycia: §2" + uses);
-                            sender.sendMessage("§6Kod do usługi: §2#" + offerid + " §c(§9" + offername + "§c)");
-                            sender.sendMessage("§6Oferta ograniczona czasowo?: §2" + timedText);
-                            if (timed == 1) {
-                                sender.sendMessage("§6Oferta ważna do: §2" + end);
-                            }
-
-                            return true;
-                        } catch (SQLException e) {
-                            e.printStackTrace();
-                            sender.sendMessage("§cWystąpił błąd w trakcie łączenia z bazą danych");
-                            return true;
-                        }
-                    }
-                }
-            } else {
-                sender.sendMessage("§cNie masz uprawnień :(");
-            }
-        }
-
-        return false;
-    }
-}
+/* Location:              C:\Users\Miłosz\Desktop\plgs\MCTrapsShop.jar!\eu\mctraps\shop\MCTrapsShopCommandExecutor.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       0.7.1
+ */
